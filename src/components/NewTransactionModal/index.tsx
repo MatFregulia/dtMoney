7ac0998725469f80e27a5e79ed333644e2ecd584
Matheus Fregulia <matheus.gviva@gmail.com>
2021-12-07 09:10:46 -0300

    Criando hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,8 +4,8 @@ import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import { FormEvent, useContext, useState } from 'react';
-import { TransactionsContext } from '../../TransactionsContext';
+import { FormEvent, useState } from 'react';
+import { useTransactions } from '../../hooks/useTransactions';
 
 
 interface NewTransactionModalPropos {
@@ -14,7 +14,7 @@ interface NewTransactionModalPropos {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalPropos) {
-    const { createTransaction } = useContext(TransactionsContext);
+    const { createTransaction } = useTransactions();
 
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0);
